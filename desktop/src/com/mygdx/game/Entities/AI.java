5a67303e3f2af3f0520b04f049cab621af97e1cc
jkoui <ooijacob2001@gmail.com>
2024-02-22 00:16:04 +0800

    Update AI.java

--- desktop/src/com/mygdx/game/Entities/AI.java
+++ desktop/src/com/mygdx/game/Entities/AI.java
@@ -12,13 +12,4 @@ public class AI extends Entity{
         this.AIObject = "whale.png"; //default AIObject
     }
 
-    public AI(String AIObject) {
-    	super(AIObject + ".png", (int) (Math.random() * 800), (int) (Math.random() * 500));
-        this.AIObject = AIObject;
-
-        //System.out.println("Created AI object: " + AIObject);
-        //System.out.print("Situational Radius: " + situationalRadius);
-        //System.out.println(" X: " + this.getPosX() + " Y: " + this.getPosY());
-    }
-
 }
