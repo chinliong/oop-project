7834e0c838cd8697abdbeeb3e067403b1f203fcb
jkoui <ooijacob2001@gmail.com>
2024-02-22 12:07:21 +0800

    update

--- desktop/src/com/mygdx/game/Managers/AIControlManager.java
+++ desktop/src/com/mygdx/game/Managers/AIControlManager.java
@@ -1,10 +1,8 @@
 package com.mygdx.game.Managers;
 
 import com.badlogic.gdx.Gdx;
-
 import com.mygdx.game.AIControl.Move;
-import com.mygdx.game.AIControl.MoveHorizontal;
-import com.mygdx.game.AIControl.MoveVertical;
+import com.mygdx.game.AIControl.Directions;
 import com.mygdx.game.Entities.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,34 +11,20 @@ import java.util.Random;
 public class AIControlManager {
 	
     private List<AI> AIList;
-    private AI entityAI;
-    private MoveVertical moveVertical;
-    private MoveHorizontal moveHorizontal;
+    private Directions directions;
     
-    public AIControlManager(List<AI> AIList){
-        this.AIList = AIList;
-    }
+   // public AIControlManager(List<AI> AIList){
+     //   this.AIList = AIList;
+    //}
     
-    // Constructor for managing a single AI entity with vertical and horizontal movement capabilities
-    public AIControlManager(AI entityAI){
-        this.moveVertical = new MoveVertical();
-        this.moveHorizontal = new MoveHorizontal();
-    }
-    
-    // Default constructor initializing vertical and horizontal movement strategies without specific AI entities
+    //constructor initializing vertical and horizontal movement for AI list
     public AIControlManager(){
         this.AIList = null;
-        this.moveVertical = new MoveVertical();
-        this.moveHorizontal = new MoveHorizontal();
-    }
-    
-    // Returns the vertical movement strategy
-    public MoveVertical getMoveVertical(){
-        return this.moveVertical;
+        this.directions = new Directions();
     }
     
     // Returns the horizontal movement strategy
-    public MoveHorizontal getMoveHorizontal(){
-        return this.moveHorizontal;
+    public Directions getMoveHorizontal(){
+        return this.directions;
     }
 }
