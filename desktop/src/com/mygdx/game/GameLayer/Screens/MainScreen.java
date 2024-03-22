58be6252f10fbc891fa6d36b4027663973c6999e
bberniicee <2302020@sit.singaporetech.edu.sg>
2024-03-22 17:15:27 +0800

    mainscreen io

--- desktop/src/com/mygdx/game/GameLayer/Screens/MainScreen.java
+++ desktop/src/com/mygdx/game/GameLayer/Screens/MainScreen.java
@@ -16,99 +16,127 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 
+public class MainScreen extends BaseScreen {
 
-public class MainScreen extends BaseScreen { 
-	
 	private Sprite backgroundSprite;
 
-    public MainScreen(SimulationLifeCycleManager game) {
-        super(game);
-        initialiseUI();
-        
-        Texture backgroundTexture = new Texture(Gdx.files.internal("forest.jpg"));
-        backgroundSprite = new Sprite(backgroundTexture);
-        backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        
-        }
+	private float newGameButtonX, newGameButtonY, newGameButtonWidth = 160, newGameButtonHeight = 60;
+	private float exitGameButtonX, exitGameButtonY, exitGameButtonWidth = 160, exitGameButtonHeight = 60;
 
-    @Override
-    public void show() {
-        //set stage as input processor, set volume, play volume and loop it
-        super.show();
-       startAudio("MainMenu", 0.5f);
-       
-    }
-    
-    @Override
-    public void render(float delta) {
-    	super.render(delta);
-    	
-    	SpriteBatch batch = game.getBatch();
-        batch.begin();
-        backgroundSprite.draw(batch);
-        batch.end();
-        
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-        stage.draw();
-    	
-    }
+	public MainScreen(SimulationLifeCycleManager game) {
+		super(game);
+		initialiseUI();
 
-    //Setup UI
-    @Override
-    protected void initialiseUI() {
-    	
-    	Texture newGameTexture = new Texture(Gdx.files.internal("startbutton.png"));
-    	Texture exitGameTexture = new Texture(Gdx.files.internal("backbutton.png"));
-    	
-    	Drawable newGameDrawable = new TextureRegionDrawable(new TextureRegion(newGameTexture));
-    	Drawable newGameHoverDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("startbuttonhover.png"))));
-    	Drawable exitGameDrawable = new TextureRegionDrawable(new TextureRegion(exitGameTexture));
-    	Drawable exitGameHoverDrawable = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("backbuttonhover.png"))));
-    	
-    	TextButton newGameButton = new TextButton("", new TextButton.TextButtonStyle( newGameDrawable, newGameDrawable, newGameDrawable, game.getFont()));
-    	TextButton exitGameButton = new TextButton("", new TextButton.TextButtonStyle( exitGameDrawable, exitGameDrawable, exitGameDrawable, game.getFont()));
-    	
-    	newGameButton.setSize( 160, 60);
-    	exitGameButton.setSize( 160, 60);
-    	
-    	newGameButton.setPosition((Gdx.graphics.getWidth() - newGameButton.getWidth()) / 2, (Gdx.graphics.getHeight() + newGameButton.getHeight()) / 2);
-    	exitGameButton.setPosition((Gdx.graphics.getWidth() - exitGameButton.getWidth()) / 2, (Gdx.graphics.getHeight() + exitGameButton.getHeight()) / 2 - 50);
-    	
-    	newGameButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-            	game.getSceneManager().setScreen(game.getSceneManager().getScreen(PlayScreen.class));
-            	game.getAudioManager().getMusic("MainMenu").stop();
-            }
-        });
-    	
-    	exitGameButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-            	Gdx.app.exit();
-            }
-        });
-    	
-    	newGameButton.getStyle().over = newGameHoverDrawable;
-    	exitGameButton.getStyle().over = exitGameHoverDrawable;
+		Texture backgroundTexture = new Texture(Gdx.files.internal("forest.jpg"));
+		backgroundSprite = new Sprite(backgroundTexture);
+		backgroundSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-        stage.addActor(newGameButton);
-        stage.addActor(exitGameButton);
-        
-        }
-    
-    @Override
-    public void resize(int width, int height) {
-        super.resize(width, height);
+		// Initialize button positions
+		newGameButtonX = (Gdx.graphics.getWidth() - newGameButtonWidth) / 2;
+		newGameButtonY = (Gdx.graphics.getHeight() + newGameButtonHeight) / 2;
+		exitGameButtonX = newGameButtonX;
+		exitGameButtonY = newGameButtonY - 50 - newGameButtonHeight; 
 
-        backgroundSprite.setSize(width, height);
-        stage.getViewport().update(width, height, true);
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-    }
+	}
+
+	@Override
+	public void show() {
+		// set stage as input processor, set volume, play volume and loop it
+		super.show();
+		startAudio("MainMenu", 0.5f);
+
+	}
+
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+
+		SpriteBatch batch = game.getBatch();
+		batch.begin();
+		backgroundSprite.draw(batch);
+		batch.end();
+
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+		stage.draw();
+
+		handleCustomInput();
+
+	}
+
+	private void handleCustomInput() {
+		float mouseX = Gdx.input.getX();
+		float mouseY = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+		if (game.getInputOutputManager().getInputMouse().ifLMBPressed()) {
+			if (mouseX >= newGameButtonX && mouseX <= newGameButtonX + newGameButtonWidth && mouseY >= newGameButtonY
+					&& mouseY <= newGameButtonY + newGameButtonHeight) {
+				// New Game button logic
+				game.getSceneManager().setScreen(game.getSceneManager().getScreen(PlayScreen.class));
+				game.getAudioManager().getMusic("MainMenu").stop();
+			} else if (mouseX >= exitGameButtonX && mouseX <= exitGameButtonX + exitGameButtonWidth
+					&& mouseY >= exitGameButtonY && mouseY <= exitGameButtonY + exitGameButtonHeight) {
+				// Exit button logic
+				Gdx.app.exit();
+			}
+		}
+	}
+
+	// Setup UI
+	@Override
+	protected void initialiseUI() {
+
+		Texture newGameTexture = new Texture(Gdx.files.internal("startbutton.png"));
+		Texture exitGameTexture = new Texture(Gdx.files.internal("backbutton.png"));
+
+		Drawable newGameDrawable = new TextureRegionDrawable(new TextureRegion(newGameTexture));
+		Drawable newGameHoverDrawable = new TextureRegionDrawable(
+				new TextureRegion(new Texture(Gdx.files.internal("startbuttonhover.png"))));
+		Drawable exitGameDrawable = new TextureRegionDrawable(new TextureRegion(exitGameTexture));
+		Drawable exitGameHoverDrawable = new TextureRegionDrawable(
+				new TextureRegion(new Texture(Gdx.files.internal("backbuttonhover.png"))));
+
+		TextButton newGameButton = new TextButton("",
+				new TextButton.TextButtonStyle(newGameDrawable, newGameDrawable, newGameDrawable, game.getFont()));
+		TextButton exitGameButton = new TextButton("",
+				new TextButton.TextButtonStyle(exitGameDrawable, exitGameDrawable, exitGameDrawable, game.getFont()));
+
+		newGameButton.setSize(160, 60);
+		exitGameButton.setSize(160, 60);
+
+		newGameButton.setPosition((Gdx.graphics.getWidth() - newGameButton.getWidth()) / 2,
+				(Gdx.graphics.getHeight() + newGameButton.getHeight()) / 2);
+		exitGameButton.setPosition((Gdx.graphics.getWidth() - exitGameButton.getWidth()) / 2,
+				(Gdx.graphics.getHeight() + exitGameButton.getHeight()) / 2 - 50);
+
+//		newGameButton.addListener(new ClickListener() {
+//			@Override
+//			public void clicked(InputEvent event, float x, float y) {
+//				game.getSceneManager().setScreen(game.getSceneManager().getScreen(PlayScreen.class));
+//				game.getAudioManager().getMusic("MainMenu").stop();
+//			}
+//		});
+
+//		exitGameButton.addListener(new ClickListener() {
+//			@Override
+//			public void clicked(InputEvent event, float x, float y) {
+//				Gdx.app.exit();
+//			}
+//		});
+
+		newGameButton.getStyle().over = newGameHoverDrawable;
+		exitGameButton.getStyle().over = exitGameHoverDrawable;
+
+		stage.addActor(newGameButton);
+		stage.addActor(exitGameButton);
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+
+		backgroundSprite.setSize(width, height);
+		stage.getViewport().update(width, height, true);
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+	}
 }
-    
-
-
-
-
-
