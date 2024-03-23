b7eec8f87046a414543a8105c887397665323d4f
bberniicee <2302020@sit.singaporetech.edu.sg>
2024-03-23 18:44:56 +0800

    pausescreen io

--- desktop/src/com/mygdx/game/GameLayer/Screens/PauseScreen.java
+++ desktop/src/com/mygdx/game/GameLayer/Screens/PauseScreen.java
@@ -22,32 +22,32 @@ public class PauseScreen extends BaseScreen {
 
     @Override
     protected void initialiseUI() {
-    	createButton("Resume", 100, 100, new ClickListener() {
-            @Override
-            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-            	game.getAudioManager().getMusic("Gameplay").play();
-                game.getSceneManager().setScreen(game.getSceneManager().getScreen(PlayScreen.class));
-            }
-        });
-
-        createButton("Main Menu", 100, 200, new ClickListener() {
-            @Override
-            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-                game.getEntityManager().disposeEntities();
-                //remove the play screen from the list of screens
-                game.getSceneManager().removeScreen(PlayScreen.class);
-                game.getSceneManager().setScreen(game.getSceneManager().getScreen(MainScreen.class));
-
-            }
-        });
-
-
-        createButton("Exit", 100, 300, new ClickListener() {
-            @Override
-            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-                Gdx.app.exit();
-            }
-        });
+//    	createButton("Resume", 100, 100, new ClickListener() {
+//            @Override
+//            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+//            	game.getAudioManager().getMusic("Gameplay").play();
+//                game.getSceneManager().setScreen(game.getSceneManager().getScreen(PlayScreen.class));
+//            }
+//        });
+//
+//        createButton("Main Menu", 100, 200, new ClickListener() {
+//            @Override
+//            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+//                game.getEntityManager().disposeEntities();
+//                //remove the play screen from the list of screens
+//                game.getSceneManager().removeScreen(PlayScreen.class);
+//                game.getSceneManager().setScreen(game.getSceneManager().getScreen(MainScreen.class));
+//
+//            }
+//        });
+//
+//
+//        createButton("Exit", 100, 300, new ClickListener() {
+//            @Override
+//            public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
+//                Gdx.app.exit();
+//            }
+//        });
 
     }
 
